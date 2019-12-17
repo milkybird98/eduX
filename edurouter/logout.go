68227975efe9217a30c36112b688fa44f9e722a9
milkybird98 <dengtimmycn@gmail.com>
2019-12-17 17:22:32 +0800

    + logout router

--- edurouter/logout.go
+++ edurouter/logout.go
@@ -1,2 +1,99 @@
 package edurouter
 
+import (
+	"fmt"
+	"eduX/utils"
+	"crypto/md5"
+	"encoding/json"
+	"eduX/eduiface"
+	"eduX/edunet"
+)
+
+var logoutFlag bool
+
+type LogoutRouter struct {
+	edunet.BaseRouter
+}
+
+type LogoutData struct {
+	uid				string
+}
+
+func (this *LogoutRouter) Handle(request eduiface.IRequest) {
+	var reqMsgInJson ReqMsg
+	var reqDataInJson LogoutData
+	var replyMsg ResMsg
+
+	reqMsgOrigin := request.GetData()
+	logoutFlag = false
+	checksumFlag = false
+
+	err := json.Unmarshal(reqMsgOrigin,&reqMsgInJson)
+	if err!=nil{
+		fmt.Println(err)
+		checksumFlag = false
+		replyMsg.status="json_format_error"
+	}
+
+	md5Ctx := md5.New()
+	md5Ctx.Write([]byte(reqMsgInJson.uid))
+  md5Ctx.Write(reqMsgInJson.data)
+
+	if utils.SliceEqual(reqMsgInJson.checksum,md5Ctx.Sum(nil)){
+		checksumFlag = true
+	}else{
+		checksumFlag = false
+		replyMsg.status="check_sum_error"
+	}
+
+	if checksumFlag {
+		err = json.Unmarshal(reqMsgInJson.data,&reqDataInJson)
+		if err!=nil{
+			fmt.Println(err)
+			checksumFlag = false
+			replyMsg.status="json_format_error"
+		}
+	}
+
+	c := request.GetConnection()
+	value,err := c.GetSession("isLogined")
+	if err!= nil {
+		checksumFlag = false
+		replyMsg.status="session_error"
+	}
+
+	if checksumFlag {
+		if value == false{
+			checksumFlag = false
+			replyMsg.status="not_login"
+		}
+	}
+
+	if checksumFlag{
+		if reqDataInJson.uid == reqMsgInJson.uid{
+			replyMsg.status="logout_success"
+			logoutFlag = true
+		}
+	}
+
+	md5Ctx = md5.New()
+	md5Ctx.Write([]byte(replyMsg.status))
+	md5Ctx.Write(replyMsg.data)
+	replyMsg.checksum = md5Ctx.Sum(nil)
+
+	jsonMsg,err :=json.Marshal(replyMsg)
+	if err!= nil{
+		fmt.Println(err)
+		return
+	}
+
+	c.SendMsg(request.GetMsgID(),jsonMsg)
+}
+
+func (this *LogoutRouter) PostHandle(request eduiface.IRequest){ 
+	if logoutFlag {
+		c := request.GetConnection()
+		c.SetSession("isLogined",false)
+		c.Stop()
+	}
+}
