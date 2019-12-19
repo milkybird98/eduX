04ff695d47a90affdefa2d1cf23d9b0f027d5801
milkybird98 <dengtimmycn@gmail.com>
2019-12-19 22:22:59 +0800

    > rewrite connection check router

--- edurouter/conn-check.go
+++ edurouter/conn-check.go
@@ -1,88 +1,52 @@
 package edurouter
 
 import (
-	"crypto/md5"
 	"eduX/eduiface"
 	"eduX/edunet"
-	"eduX/utils"
-	"encoding/json"
+
 	"fmt"
+
+	"github.com/tidwall/gjson"
 )
 
 type PingRouter struct {
 	edunet.BaseRouter
 }
 
-type PingData struct {
-	ping string
-}
-
 var conncheck_replyStatus string
 
 func (this *PingRouter) PreHandle(request eduiface.IRequest) {
-	var reqMsgInJSON ReqMsg
-	var reqDataInJson PingData
-
-	conncheck_replyStatus = ""
-	reqMsgOrigin := request.GetData()
-
-	checksumFlag = false
-
-	err := json.Unmarshal(reqMsgOrigin, &reqMsgInJSON)
-	if err != nil {
-		fmt.Println(err)
-		conncheck_replyStatus = "json_format_error"
+	reqMsgInJSON, conncheck_replyStatus, ok := CheckMsgFormat(request)
+	if ok != true {
+		fmt.Println("PingRouter: ", conncheck_replyStatus)
 		return
 	}
 
-	md5Ctx := md5.New()
-	md5Ctx.Write([]byte(reqMsgInJSON.uid))
-	md5Ctx.Write(reqMsgInJSON.data)
-
-	if utils.SliceEqual(reqMsgInJSON.checksum, md5Ctx.Sum(nil)) {
-		checksumFlag = true
-	} else {
-		conncheck_replyStatus = "check_sum_error"
+	conncheck_replyStatus, ok = CheckConnectionLogin(request)
+	if ok != true {
 		return
 	}
 
-	err = json.Unmarshal(reqMsgInJSON.data, &reqDataInJson)
-	if err != nil {
-		fmt.Println(err)
-		conncheck_replyStatus = "json_format_error"
+	pingData := gjson.GetBytes(reqMsgInJSON.Data, "ping")
+	if !pingData.Exists() {
+		conncheck_replyStatus = "data_format_error"
 		return
 	}
 
-	c := request.GetConnection()
-	value, err := c.GetSession("isLogined")
-	if err != nil {
-		conncheck_replyStatus = "session_error"
-		return
-	}
+	reqPing := pingData.String()
 
-	if value == false {
-		conncheck_replyStatus = "not_login"
-		return
-	}
-
-	if reqDataInJson.ping == "ping" {
+	if reqPing == "ping" {
 		conncheck_replyStatus = "pong"
+	} else {
+		conncheck_replyStatus = "data_format_error"
 	}
 }
 
 func (this *PingRouter) Handle(request eduiface.IRequest) {
-	var replyMsg ResMsg
-
-	replyMsg.status = conncheck_replyStatus
-
-	md5Ctx := md5.New()
-	md5Ctx.Write([]byte(replyMsg.status))
-	md5Ctx.Write(replyMsg.data)
-	replyMsg.checksum = md5Ctx.Sum(nil)
-
-	jsonMsg, err := json.Marshal(replyMsg)
+	fmt.Println("PingRouter: ", conncheck_replyStatus)
+	jsonMsg, err := CombineReplyMsg(conncheck_replyStatus, nil)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("PingRouter: ", err)
 		return
 	}
 
