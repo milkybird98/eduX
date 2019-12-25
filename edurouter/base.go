06a8c409a7bf99237f9f4d5f8ea853a622c7601f
milkybird98 <dengtimmycn@gmail.com>
2019-12-25 20:25:16 +0800

    * code format | + check request uid is right

--- edurouter/base.go
+++ edurouter/base.go
@@ -4,26 +4,29 @@ import (
 	"crypto/md5"
 	"eduX/eduiface"
 	"eduX/utils"
-	"encoding/json"
 	"encoding/base64"
+	"encoding/json"
 
 	"github.com/tidwall/gjson"
 )
 
 var checksumFlag bool
 
+//ReqMsg 接收数据结构体
 type ReqMsg struct {
 	UID      string `json:"uid"`
-	Data     []byte	`json:"data"`
-	CheckSum string	`json:"checksum"`
+	Data     []byte `json:"data"`
+	CheckSum string `json:"checksum"`
 }
 
+//ResMsg 发送数据结构体
 type ResMsg struct {
-	Status   string	`json:"status"`
-	Data     []byte	`json:"data"`
-	Checksum string	`json:"checksum"`
+	Status   string `json:"status"`
+	Data     []byte `json:"data"`
+	Checksum string `json:"checksum"`
 }
 
+//CheckMsgFormat 检查接收数据格式是否正确
 func CheckMsgFormat(request eduiface.IRequest) (*ReqMsg, string, bool) {
 	var reqMsgInJSON ReqMsg
 	reqMsgOrigin := request.GetData()
@@ -36,16 +39,15 @@ func CheckMsgFormat(request eduiface.IRequest) (*ReqMsg, string, bool) {
 
 	parseResult := gjson.ParseBytes(reqMsgOrigin)
 
-
 	reqMsgInJSON.UID = parseResult.Get("uid").String()
 	dataInMsg := parseResult.Get("data")
 	if dataInMsg.Exists() {
 		var err error
-		reqMsgInJSON.Data,err = base64.StdEncoding.DecodeString(dataInMsg.String())
-		if err!=nil{
+		reqMsgInJSON.Data, err = base64.StdEncoding.DecodeString(dataInMsg.String())
+		if err != nil {
 			return nil, "data_base64_format_error", false
 		}
-	}else{
+	} else {
 		reqMsgInJSON.Data = nil
 	}
 	reqMsgInJSON.CheckSum = gjson.GetBytes(reqMsgOrigin, "checksum").String()
@@ -60,8 +62,18 @@ func CheckMsgFormat(request eduiface.IRequest) (*ReqMsg, string, bool) {
 	return &reqMsgInJSON, "", true
 }
 
-func CheckConnectionLogin(request eduiface.IRequest) (string, bool) {
+//CheckConnectionLogin 检查当前用户是否已登陆
+func CheckConnectionLogin(request eduiface.IRequest, UID string) (string, bool) {
 	c := request.GetConnection()
+	sessionUID, err := c.GetSession("UID")
+	if err != nil {
+		return "session_error", false
+	}
+
+	if sessionUID != UID {
+		return "uid_not_match", false
+	}
+
 	value, err := c.GetSession("isLogined")
 	if err != nil {
 		return "session_error", false
@@ -74,6 +86,7 @@ func CheckConnectionLogin(request eduiface.IRequest) (string, bool) {
 	return "", true
 }
 
+//CombineReplyMsg 拼接,计算MD5并且序列化返回数据
 func CombineReplyMsg(status string, dataInJSON interface{}) ([]byte, error) {
 	var replyMsg ResMsg
 	var err error
