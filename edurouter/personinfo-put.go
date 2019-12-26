e611fb1d72a234ab66d39ca4feab93cb02b22dfa
milkybird98 <dengtimmycn@gmail.com>
2019-12-26 22:39:33 +0800

    - remove classname from data

--- edurouter/personinfo-put.go
+++ edurouter/personinfo-put.go
@@ -14,10 +14,9 @@ type PersonInfoPutRouter struct {
 }
 
 type PersonInfoPutData struct {
-	UID       string `json:"uid"`
-	Name      string `json:"name"`
-	ClassName string `json:"class"`
-	Gender    int    `json:"gender"`
+	UID    string `json:"uid"`
+	Name   string `json:"name"`
+	Gender int    `json:"gender"`
 }
 
 var personputReplyStatus string
@@ -30,13 +29,13 @@ func (router *PersonInfoPutRouter) PreHandle(request eduiface.IRequest) {
 		return
 	}
 
-	classjoiningetReplyStatus, ok = CheckConnectionLogin(request, reqMsgInJSON.UID)
+	personputReplyStatus, ok = CheckConnectionLogin(request, reqMsgInJSON.UID)
 	if ok != true {
 		return
 	}
 
 	if !gjson.Valid(string(reqMsgInJSON.Data)) {
-		classjoiningetReplyStatus = "data_format_error"
+		personputReplyStatus = "data_format_error"
 		return
 	}
 
@@ -44,52 +43,51 @@ func (router *PersonInfoPutRouter) PreHandle(request eduiface.IRequest) {
 	newPersonInfoData := gjson.ParseBytes(reqMsgInJSON.Data)
 	reqDataInJSON.UID = newPersonInfoData.Get("uid").String()
 	reqDataInJSON.Name = newPersonInfoData.Get("name").String()
-	reqDataInJSON.ClassName = newPersonInfoData.Get("class").String()
 	reqDataInJSON.Gender = int(newPersonInfoData.Get("gender").Int())
 
 	//权限检查
 	c := request.GetConnection()
 	sessionUID, err := c.GetSession("UID")
 	if err != nil {
-		persongetReplyStatus = "session_error"
+		personputReplyStatus = "session_error"
 		return
 	}
 
 	userData := edumodel.GetUserByUID(reqDataInJSON.UID)
 	if userData == nil {
-		persongetReplyStatus = "user_not_found"
+		personputReplyStatus = "user_not_found"
 		return
 	}
 
 	if sessionUID != reqDataInJSON.UID {
-		sessionPlace, err := c.GetSession("plcae")
+		sessionPlace, err := c.GetSession("place")
 		if err != nil {
-			persongetReplyStatus = "session_error"
+			personputReplyStatus = "session_error"
 			return
 		}
 
 		if sessionPlace == "student" {
-			persongetReplyStatus = "permission_error"
+			personputReplyStatus = "permission_error"
 			return
 		} else if sessionPlace == "teacher" {
-			sessionClass, err := c.GetSession("Class")
+			sessionClass, err := c.GetSession("class")
 			if err != nil {
-				persongetReplyStatus = "session_error"
+				personputReplyStatus = "session_error"
 				return
 			}
 			if userData.Class != sessionClass {
-				persongetReplyStatus = "permission_error"
+				personputReplyStatus = "permission_error"
 				return
 			}
 		}
 	}
 
 	//修改个人信息
-	res := edumodel.UpdateUserByID(reqMsgInJSON.UID, reqDataInJSON.ClassName, reqDataInJSON.Name, "", reqDataInJSON.Gender)
+	res := edumodel.UpdateUserByID(reqDataInJSON.UID, "", reqDataInJSON.Name, "", reqDataInJSON.Gender)
 	if res {
-		personputReplyStatus = "update_success"
+		personputReplyStatus = "success"
 	} else {
-		personputReplyStatus = "update_fail"
+		personputReplyStatus = "model_fail"
 	}
 }
 
