61960ed8d35a22b07077fb730715931dd02caaa7
milkybird98 <dengtimmycn@gmail.com>
2019-12-31 20:20:19 +0800

    + add notes

--- edurouter/login.go
+++ edurouter/login.go
@@ -12,73 +12,89 @@ import (
 	"github.com/tidwall/gjson"
 )
 
+// LoginRouter 处理用户登陆请求
 type LoginRouter struct {
 	edunet.BaseRouter
 }
 
+// LoginData 定义登陆请求数据的参数
 type LoginData struct {
 	Pwd string `json:"pwd"`
 }
 
+// 回复状态码
 var loginReplyStatus string
 
 // PreHandle 用于进行原始数据校验,权限验证,身份验证,数据获取和数据库操作
 func (router *LoginRouter) PreHandle(request eduiface.IRequest) {
 	var reqMsgInJSON *ReqMsg
 	var ok bool
+	// 试图解码原始数据
 	reqMsgInJSON, loginReplyStatus, ok = CheckMsgFormat(request)
 	if ok != true {
 		return
 	}
 
+	// 检查当前连接是否已登录
 	userData := edumodel.GetUserByUID(reqMsgInJSON.UID)
 	if userData == nil {
 		loginReplyStatus = "login_fail"
 		return
 	}
 
+	// 尝试从请求数据Data段获取密码数据
 	loginData := gjson.GetBytes(reqMsgInJSON.Data, "pwd")
+	// 若数据不存在,则返回错误码
 	if !loginData.Exists() {
 		loginReplyStatus = "data_format_error"
 		return
 	}
 
+	// 密码去盐
 	pwdInByte := []byte(loginData.String())
 	pwdInDecode, err := PwdRemoveSalr(pwdInByte)
+	// 若密码解码失败,则返回错误码
 	if err != nil {
 		loginReplyStatus = "pwd_format_error"
 		return
 	}
 
+	// 从数据库查询授权数据
 	authData := edumodel.GetUserAuthByUID(reqMsgInJSON.UID)
+	// 如果授权数据不存在则返回错误码
 	if authData == nil {
 		loginReplyStatus = "login_fail"
 		return
 	}
 
-	//fmt.Println([]byte(pwdInDecode))
-	//fmt.Println([]byte(authData.Pwd))
+	// 如果认证成功,则返回success
 	if pwdInDecode == authData.Pwd {
 		loginReplyStatus = "success"
 		c := request.GetConnection()
 
+		// 修改session
 		c.SetSession("isLogined", true)
 		c.SetSession("UID", userData.UID)
 		c.SetSession("place", userData.Place)
 	} else {
+		// 否则返回错误码
 		loginReplyStatus = "fail"
 	}
 }
 
 // Handle 用于将请求的处理结果发回客户端
 func (router *LoginRouter) Handle(request eduiface.IRequest) {
+	// 打印请求处理Log
 	fmt.Println("[ROUTER] ", time.Now().In(utils.GlobalObject.TimeLocal).Format(utils.GlobalObject.TimeFormat), ", Client Address: ", request.GetConnection().GetTCPConnection().RemoteAddr(), ", LoginRouter: ", loginReplyStatus)
+	// 生成返回数据
 	jsonMsg, err := CombineReplyMsg(loginReplyStatus, nil)
+	// 如果生成失败则报错返回
 	if err != nil {
 		fmt.Println("LoginRouter: ", err)
 		return
 	}
 
+	// 发送返回数据
 	c := request.GetConnection()
 	c.SendMsg(request.GetMsgID(), jsonMsg)
 }
--- edurouter/logout.go
+++ edurouter/logout.go
@@ -8,42 +8,52 @@ import (
 	"time"
 )
 
+// LogoutRouter 处理用户登出请求
 type LogoutRouter struct {
 	edunet.BaseRouter
 }
 
+// 返回状态码
 var logoutReplyStatus string
 
 // PreHandle 用于进行原始数据校验,权限验证,身份验证,数据获取和数据库操作
 func (router *LogoutRouter) PreHandle(request eduiface.IRequest) {
 	var reqMsgInJSON *ReqMsg
 	var ok bool
+	// 试图解码原始数据
 	reqMsgInJSON, logoutReplyStatus, ok = CheckMsgFormat(request)
 	if ok != true {
 		return
 	}
 
+	// 检查当前连接是否已登录
 	logoutReplyStatus, ok = CheckConnectionLogin(request, reqMsgInJSON.UID)
 	if ok != true {
 		return
 	}
 
+	// 返回success
 	logoutReplyStatus = "success"
 }
 
 // Handle 用于将请求的处理结果发回客户端
 func (router *LogoutRouter) Handle(request eduiface.IRequest) {
+	// 打印请求处理Log
 	fmt.Println("[ROUTER] ", time.Now().In(utils.GlobalObject.TimeLocal).Format(utils.GlobalObject.TimeFormat), ", Client Address: ", request.GetConnection().GetTCPConnection().RemoteAddr(), ", LogoutRouter: ", logoutReplyStatus)
+	// 生成返回数据
 	jsonMsg, err := CombineReplyMsg(logoutReplyStatus, nil)
+	// 如果生成失败则报错返回
 	if err != nil {
 		fmt.Println("LogoutRouter: ", err)
 		return
 	}
 
+	// 发送返回数据
 	c := request.GetConnection()
 	c.SendMsg(request.GetMsgID(), jsonMsg)
 }
 
+// PostHandle 用于在返回数据后关闭连接
 func (router *LogoutRouter) PostHandle(request eduiface.IRequest) {
 	if logoutReplyStatus == "success" {
 		c := request.GetConnection()
